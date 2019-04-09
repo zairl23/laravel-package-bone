26f2d580448bae0962972a9fc685c23d569ee87c
zhanglei1285 <zhanglei1285@stnts.com>
2019-04-09 19:30:52 +0800

    Fix setup problem and delete the composer.lock file

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -3,28 +3,27 @@
 namespace Tests;
 
 use Orchestra\Testbench\TestCase;
-use Illuminate\Queue\WorkerOptions;
-use Illuminate\Support\Facades\Redis;
 
 /**
- * IntegrationTest
+ * IntegrationTest.
  *
  * @category TestCase
- * @package  Tests
+ *
  * @author   ney <zoobile@gmail.com>
  * @license  MIT https://github.com/swooliy/laraket/me/LICENSE.md
- * @link     https://github.com/swooliy/laraket/me
+ *
+ * @see     https://github.com/swooliy/laraket/me
  */
 abstract class IntegrationTest extends TestCase
 {
     /**
      * Setup the test case.
-     *
-     * @return void
      */
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->app['config']->set('app.key', 'base64:UTyp33UhGolgzCK5CJmT+hNHcA+dJyp3+oINtX+VoPI=');
 
         // $this->loadLaravelMigrations(['--database' => 'testing']);
 
@@ -35,20 +34,19 @@ abstract class IntegrationTest extends TestCase
 
     /**
      * Tear down the test case.
-     *
-     * @return void
      */
     protected function tearDown(): void
     {
-        parent::tearDown();
-
         $this->artisan('migrate:reset', ['--database' => 'testing'])->run();
+
+        parent::tearDown();
     }
 
     /**
      * Get the service providers for the package.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param \Illuminate\Foundation\Application $app
+     *
      * @return array
      */
     protected function getPackageProviders($app)
@@ -58,33 +56,31 @@ abstract class IntegrationTest extends TestCase
         ];
     }
 
-     /**
+    /**
      * Override application aliases.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param \Illuminate\Foundation\Application $app
      *
      * @return array
      */
     protected function getApplicationAliases($app)
     {
         return [
-            
         ];
     }
 
     /**
      * Configure the environment.
      *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
+     * @param \Illuminate\Foundation\Application $app
      */
     protected function getEnvironmentSetUp($app): void
     {
         $app['config']->set('database.default', 'testing');
         $app['config']->set('database.connections.testing', [
-            'driver'   => 'sqlite',
+            'driver' => 'sqlite',
             'database' => ':memory:',
-            'prefix'   => '',
+            'prefix' => '',
         ]);
     }
 }
